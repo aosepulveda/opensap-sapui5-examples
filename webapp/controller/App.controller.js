856d742a189a1bf77e1d75796d007d7162b66d50
Alejandro Oyarzún <aosepulveda@outlook.com>
2016-06-02 02:00:56 +0000

    Agregado i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,7 +7,11 @@ sap.ui.define([
 	return Controller.extend("opensap.myapp.controller.App", {
 		
 		onShowHello: function() {
-			MessageToast.show("Hello openSAP");
+			//
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			MessageToast.show(sMsg);
 		}
 	});
 });
