a7af1ec1d29940def74355723dd91979d5b1d24a
Alejandro Oyarzún <aosepulveda@outlook.com>
2016-06-02 01:15:58 +0000

    Agregado Component.js y manifest.json

---
+++ webapp/Component.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+	
+	return UIComponent.extend("opensap.myapp.Component", {
+		
+		metadata: {
+			manifest: "json"
+		},
+		
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
+			
+			// Agregar cosas extras en init.
+		} 
+	});
+});
