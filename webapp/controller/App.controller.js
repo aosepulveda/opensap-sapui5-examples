5e209e4c5480a0a77aacf6adb5a32c1fe672986b
Alejandro Oyarzún <aosepulveda@outlook.com>
2016-06-06 23:29:54 +0000

    Filtros y Grupos

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"opensap/myapp/model/formatter"
-], function(Controller, MessageToast, formatter) {
+], function(Controller, MessageToast, Filter, FilterOperator, formatter) {
 	"use strict";
 	
 	return Controller.extend("opensap.myapp.controller.App", {
@@ -14,6 +16,18 @@ sap.ui.define([
 			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			MessageToast.show(sMsg);
+		},
+
+		onFilterProducts : function (oEvent) {
+			var aFilter = [], sQuery = oEvent.getParameter("query"),
+				oList = this.getView().byId("productsList"),
+				oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			
+			oBinding.filter(aFilter);
 		}
 	});
 });
